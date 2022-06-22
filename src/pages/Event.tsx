745ea6e24261cc60b129301103c26bb0dbf6a866
Bruno Domingues <brunood07@gmail.com>
2022-06-21 23:43:27 -0300

    feat: class 2 finished

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,5 +1,4 @@
 import { Header } from "../components/Header";
-import { Lesson } from "../components/Lesson";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
